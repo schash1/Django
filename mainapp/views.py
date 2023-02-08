88205c6817ac52a3c10f039e6f02bca84f9dc759
Софья Чащихина <105418002+schash1@users.noreply.github.com>
2023-02-08 11:03:47 +0300

    Update views.py

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,8 +1,10 @@
 from django.http import HttpResponse
+from django.views.generic import View
 
 
-def hello_world(request):
-    return HttpResponse("Hello world")
+class HelloWorldView(View):
+    def get(self, *args, **kwargs):
+        return HttpResponse("Hello world")
 
 
 def check_kwargs(request, **kwargs):
