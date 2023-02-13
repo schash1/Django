5ea61d139abe5bf8649f01d10202d50285728eb7
Софья Чащихина <105418002+schash1@users.noreply.github.com>
2023-02-13 13:46:04 +0300

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -88,3 +88,14 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
+
+INSTALLED_APPS = [
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
+    "markdownify.apps.MarkdownifyConfig",
+    "mainapp",
+]
