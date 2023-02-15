6e0d3c292dcdab82b29b769ed448e5313c7fc105
Софья Чащихина <105418002+schash1@users.noreply.github.com>
2023-02-15 14:55:12 +0300

    Update admin.py

--- mainapp/admin.py
+++ mainapp/admin.py
@@ -3,6 +3,6 @@ from django.contrib import admin
 from mainapp import models as mainapp_models
 
 
-@admin.register(mainapp_models.News)
-class NewsAdmin(admin.ModelAdmin):
-    pass
+@admin.register(mainapp_models.Lesson)
+class LessonAdmin(admin.ModelAdmin):
+    list_display = ["id", "num", "title", "deleted"]
