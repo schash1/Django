1fbd9a6d29c9db82a3457b320b721a8f99fd81c0
Софья Чащихина <105418002+schash1@users.noreply.github.com>
2023-02-15 14:55:46 +0300

    Update admin.py

--- mainapp/admin.py
+++ mainapp/admin.py
@@ -1,8 +1,19 @@
 from django.contrib import admin
+from django.utils.translation import gettext_lazy as _
 
 from mainapp import models as mainapp_models
 
 
+@admin.register(mainapp_models.News)
+class NewsAdmin(admin.ModelAdmin):
+    pass
+
+
 @admin.register(mainapp_models.Lesson)
 class LessonAdmin(admin.ModelAdmin):
-    list_display = ["id", "num", "title", "deleted"]
+    list_display = ["id", "get_course_name", "num", "title", "deleted"]
+
+    def get_course_name(self, obj):
+        return obj.course.name
+
+    get_course_name.short_description = _("Course")
