ff6796eaad7c82b1749c5b0279716058b4ef67f6
Софья Чащихина <105418002+schash1@users.noreply.github.com>
2023-02-15 15:12:22 +0300

    Update models.py

--- mainapp/models.py
+++ mainapp/models.py
@@ -1,3 +1,4 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 from django.utils.translation import gettext_lazy as _
 
@@ -49,6 +50,19 @@ class Courses(models.Model):
         self.save()
 
 
+class CourseFeedback(models.Model):
+    RATING = ((5, "⭐⭐⭐⭐⭐"), (4, "⭐⭐⭐⭐"), (3, "⭐⭐⭐"), (2, "⭐⭐"), (1, "⭐"))
+    course = models.ForeignKey(Courses, on_delete=models.CASCADE, verbose_name=_("Course"))
+    user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE, verbose_name=_("User"))
+    feedback = models.TextField(default=_("No feedback"), verbose_name=_("Feedback"))
+    rating = models.SmallIntegerField(choices=RATING, default=5, verbose_name=_("Rating"))
+    created = models.DateTimeField(auto_now_add=True, verbose_name="Created")
+    deleted = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"{self.course} ({self.user})"
+
+
 class Lesson(models.Model):
     course = models.ForeignKey(Courses, on_delete=models.CASCADE)
     num = models.PositiveIntegerField(verbose_name="Lesson number")
@@ -89,129 +103,3 @@ class CourseTeachers(models.Model):
     class Meta:
         verbose_name = _("Teacher")
         verbose_name_plural = _("Teachers")
-
-  </div>
-
-  <!-- Footer -->
-
-  <div class="container-lg mt-auto">
-    <hr>
-    <div class="row justify-content-center">
-      <div class="col-sm-6 col-md-3 text-center">
-        <p>
-          <strong>Braniac</strong>
-        </p>
-        <p>
-        <ul class="list-unstyled">
-          <li><a href="{% url 'mainapp:main_page' %}">Домашняя</a></li>
-          <li><a href="{% url 'mainapp:news' %}">Новости</a></li>
-          <li><a href="{% url 'mainapp:login' %}">Войти</a></li>
-        </ul>
-        </p>
-      </div>
-      <div class="col-sm-6 col-md-3 text-center">
-        <p>
-          <strong>Полезное</strong>
-        </p>
-        <p>
-        <ul class="list-unstyled">
-          <li><a href="#">Положения &amp; Условия</a></li>
-          <li><a href="#">Конфиденциальность &amp; Cookies</a></li>
-          <li><a href="#">Документация по API</a></li>
-          <li><a href="{% url 'mainapp:doc_site' %}">Документация по сайту</a>
-          </li>
-        </ul>
-        </p>
-      </div>
-      <div class="col-sm-6 col-md-3 text-center">
-        <p>
-          <strong>Мы в социальных сетях</strong>
-        </p>
-        <p>
-        <div class="row justify-content-around">
-          <div><a href="#"><i class="fab fa-vk fa-2x"></i></a></div>
-          <div><a href="#"><i class="fab fa-facebook-f fa-2x"></i></a>
-          </div>
-          <div><a href="#"><i class="fab fa-instagram fa-2x"></i></a>
-          </div>
-          <div><a href="#"><i class="fab fa-pinterest-p fa-2x"></i></a></div>
-        </div>
-        </p>
-        <p>
-          <strong>Наше приложение</strong>
-        </p>
-        <p>
-        <div class="row justify-content-around">
-          <div><a href="#"><i class="fab fa-app-store fa-2x"></i></a>
-          </div>
-          <div><a href="#"><i class="fab fa-google-play fa-2x"></i></a></div>
-          <div><a href="#"><i class="fab fa-windows fa-2x"></i></a>
-          </div>
-        </div>
-        </p>
-      </div>
-    </div>
-    <div class="row justify-content-center">
-      <div>
-        <p><small>&copy; GeekBrains 2021</small></p>
-      </div>
-    </div>
-  </div>
-
-  <!-- /Footer -->
-
-
-
-
-  <!-- JavaScript section -->
-  <!-- Bootstrap -->
-  <script src="{% static 'js/jquery-3.6.0.min.js' %}"></script>
-  <script src="{% static 'js/popper.min.js' %}"></script>
-  <script src="{% static 'js/bootstrap.min.js' %}"></script>
-
-  <!-- ChartJs -->
-  <script src="{% static 'js/Chart.min.js' %}"></script>
-
-  <!-- FontAwesome -->
-  <script src="{% static 'js/fontawesome.all.min.js' %}"></script>
-
-
-
-</body>
-
-</html>
-
-class Lesson(models.Model):
-    course = models.ForeignKey(Courses, on_delete=models.CASCADE)
-    num = models.PositiveIntegerField(verbose_name="Lesson number")
-    title = models.CharField(max_length=256, verbose_name="Name")
-    description = models.TextField(verbose_name="Description", blank=True, null=True)
-    description_as_markdown = models.BooleanField(verbose_name="As markdown", default=False)
-    created = models.DateTimeField(auto_now_add=True, verbose_name="Created", editable=False)
-    updated = models.DateTimeField(auto_now=True, verbose_name="Edited", editable=False)
-    deleted = models.BooleanField(default=False)
-
-    def __str__(self) -> str:
-        return f"{self.course.name} | {self.num} | {self.title}"
-
-    def delete(self, *args):
-        self.deleted = True
-        self.save()
-
-    class Meta:
-        ordering = ("course", "num")
-
-
-class CourseTeachers(models.Model):
-    course = models.ManyToManyField(Courses)
-    name_first = models.CharField(max_length=128, verbose_name="Name")
-    name_second = models.CharField(max_length=128, verbose_name="Surname")
-    day_birth = models.DateField(verbose_name="Birth date")
-    deleted = models.BooleanField(default=False)
-
-    def __str__(self) -> str:
-        return "{0:0>3} {1} {2}".format(self.pk, self.name_second, self.name_first)
-
-    def delete(self, *args):
-        self.deleted = True
-        self.save()
