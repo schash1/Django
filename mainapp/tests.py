9d7521f6d47854ca8f8aee1f092f7bd63b9119a9
Софья Чащихина <105418002+schash1@users.noreply.github.com>
2023-02-17 10:16:40 +0300

    Create tests.py

---
+++ mainapp/tests.py
@@ -0,0 +1,11 @@
+from http import HTTPStatus
+
+from django.test import TestCase
+from django.urls import reverse
+
+
+class TestMainPage(TestCase):
+    def test_page_open(self):
+        path = reverse("mainapp:main_page")
+        result = self.client.get(path)
+        self.assertEqual(result.status_code, HTTPStatus.OK)
